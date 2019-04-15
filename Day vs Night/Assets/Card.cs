fb68d02a687cce1d70587f80e9899da86c3d3cfb
connor014 <connor014@gmail.com>
2019-04-15 15:37:40 +1000

    card class
    card class created

---
+++ Day vs Night/Assets/Card.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Ensures that the class is accessable through the asset menu. 
+[CreateAssetMenu(fileName = "New Card", menuName = "cardTemplate")]
+
+// Creates the class that will act as a template for each card. Added by Connor.
+public class Card : ScriptableObject {
+
+    /* Store card values. */
+
+    // Stores the name of the card.
+    public string cardName;
+        
+    // Stores the description of the card.
+    public string description;
+
+    // Stores the artwork of the card. 
+    public Sprite artwork;
+
+    // Stores the attack value of the card.
+    public int attack;
+
+    // Stores the defence value of the card. 
+    public int defence;
+
+    //
+    public void Print ()
+    {
+        Debug.Log(cardName + ":" + description + "The card has:" + attack + "attack." + "The card has:" + defence + "defence.");
+    }
+
+}
---
+++ Day vs Night/Assets/cardsDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cardsDisplay : MonoBehaviour
+{
+    //
+    public Card card;
+
+    public Text nameText;
+    public Text descriptionText;
+    public Image artworkImage;
+    public Text attackText;
+    public Text defenceText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nameText.text = card.cardName;
+        descriptionText.text = card.description;
+        artworkImage.sprite = card.artwork;
+        attackText.text = card.attack.ToString();
+        defenceText.text = card.defence.ToString();
+    }
+}
